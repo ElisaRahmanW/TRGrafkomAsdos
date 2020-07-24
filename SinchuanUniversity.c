c38a91b7ec1a40cbf4b6ea83200489c066d711ce
ElisaRahmanW <68095740+ElisaRahmanW@users.noreply.github.com>
2020-07-24 10:21:22 +0700

    gedung tambahan kiri

--- SinchuanUniversity.c
+++ SinchuanUniversity.c
@@ -512,7 +512,310 @@ void tampil(void)
    glVertex3f(60,-80,-70);
    glEnd();
 	
-	
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//KIRI ABU" ATAS
+//KIRI
+   glBegin(GL_POLYGON);
+   glColor3f(0.75,0.75,0.75);
+   glVertex3f(-100,50,50);
+   glVertex3f(-100,50,70);
+   glVertex3f(-100,-50,70);
+   glVertex3f(-100,-50,50);
+   glEnd();
+//DEPAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.82,0.82,0.82);
+   glVertex3f(-100,50,70);
+   glVertex3f(-60,50,70);
+   glVertex3f(-60,-50,70);
+   glVertex3f(-100,-50,70);
+   glEnd();
+//KANAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.75,0.75,0.75);
+   glVertex3f(-60,50,70);
+   glVertex3f(-60,-50,70);
+   glVertex3f(-60,-50,50);
+   glVertex3f(-60,50,50);
+   glEnd();
+//ATAS
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-60,50,75);
+   glVertex3f(-60,50,50);
+   glVertex3f(-105,50,50);
+   glVertex3f(-105,50,75);
+   glEnd();
+//BAWAH
+   glBegin(GL_POLYGON);
+   glColor3f(0,0,1);
+   glVertex3f(-60,-50,70);
+   glVertex3f(-60,-50,50);
+   glVertex3f(-100,-50,50);
+   glVertex3f(-100,-50,70);
+   glEnd();
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//KIRI ABU" ATAS (BELAKANG)
+//KIRI
+   glBegin(GL_POLYGON);
+   glColor3f(0.75,0.75,0.75);
+   glVertex3f(-100,50,-50);
+   glVertex3f(-100,50,-70);
+   glVertex3f(-100,-50,-70);
+   glVertex3f(-100,-50,-50);
+   glEnd();
+//DEPAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.82,0.82,0.82);
+   glVertex3f(-100,50,-70);
+   glVertex3f(-60,50,-70);
+   glVertex3f(-60,-50,-70);
+   glVertex3f(-100,-50,-70);
+   glEnd();
+//KANAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.75,0.75,0.75);
+   glVertex3f(-60,50,-70);
+   glVertex3f(-60,-50,-70);
+   glVertex3f(-60,-50,-50);
+   glVertex3f(-60,50,-50);
+   glEnd();
+//ATAS
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-60,50,-75);
+   glVertex3f(-60,50,-50);
+   glVertex3f(-105,50,-50);
+   glVertex3f(-105,50,-75);
+   glEnd();
+//BAWAH
+   glBegin(GL_POLYGON);
+   glColor3f(0,0,1);
+   glVertex3f(-60,-50,-70);
+   glVertex3f(-60,-50,-50);
+   glVertex3f(-100,-50,-50);
+   glVertex3f(-100,-50,-70);
+   glEnd();
+//****************************************************************************************************************************************
+//JENDELA KIRI 1
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-90,40,70.1);
+   glVertex3f(-70,40,70.1);
+   glVertex3f(-70,20,70.1);
+   glVertex3f(-90,20,70.1);
+   glEnd();
+   glBegin(GL_LINE_LOOP);
+   glColor3f(0,0,0);
+   glVertex3f(-90,40,70.15);
+   glVertex3f(-70,40,70.15);
+   glVertex3f(-70,20,70.15);
+   glVertex3f(-90,20,70.15);
+   glEnd();
+   glBegin(GL_LINES);
+   glColor3f(0,0,0);
+   glVertex3f(-90,35,70.15);
+   glVertex3f(-70,35,70.15);
+   glVertex3f(-90,30,70.15);
+   glVertex3f(-70,30,70.15);
+   glVertex3f(-70,25,70.15);
+   glVertex3f(-90,25,70.15);
+
+   glVertex3f(-75,40,70.15);
+   glVertex3f(-75,20,70.15);
+   glVertex3f(-80,40,70.15);
+   glVertex3f(-80,20,70.15);
+   glVertex3f(-85,40,70.15);
+   glVertex3f(-85,20,70.15);
+   glEnd();
+
+//JENDELA KIRI 2
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-90,10,70.1);
+   glVertex3f(-70,10,70.1);
+   glVertex3f(-70,-10,70.1);
+   glVertex3f(-90,-10,70.1);
+   glEnd();
+   glBegin(GL_LINE_LOOP);
+   glColor3f(0,0,0);
+   glVertex3f(-90,10,70.15);
+   glVertex3f(-70,10,70.15);
+   glVertex3f(-70,-10,70.15);
+   glVertex3f(-90,-10,70.15);
+   glEnd();
+   glBegin(GL_LINES);
+   glColor3f(0,0,0);
+   glVertex3f(-90,5,70.15);
+   glVertex3f(-70,5,70.15);
+   glVertex3f(-90,0,70.15);
+   glVertex3f(-70,0,70.15);
+   glVertex3f(-70,-5,70.15);
+   glVertex3f(-90,-5,70.15);
+
+   glVertex3f(-75,10,70.15);
+   glVertex3f(-75,-10,70.15);
+   glVertex3f(-80,10,70.15);
+   glVertex3f(-80,-10,70.15);
+   glVertex3f(-85,10,70.15);
+   glVertex3f(-85,-10,70.15);
+   glEnd();
+
+//JENDELA KIRI 3
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-90,-40,70.1);
+   glVertex3f(-70,-40,70.1);
+   glVertex3f(-70,-20,70.1);
+   glVertex3f(-90,-20,70.1);
+   glEnd();
+   glBegin(GL_LINE_LOOP);
+   glColor3f(0,0,0);
+   glVertex3f(-90,-40,70.15);
+   glVertex3f(-70,-40,70.15);
+   glVertex3f(-70,-20,70.15);
+   glVertex3f(-90,-20,70.15);
+   glEnd();
+   glBegin(GL_LINES);
+   glColor3f(0,0,0);
+   glVertex3f(-90,-35,70.15);
+   glVertex3f(-70,-35,70.15);
+   glVertex3f(-90,-30,70.15);
+   glVertex3f(-70,-30,70.15);
+   glVertex3f(-70,-25,70.15);
+   glVertex3f(-90,-25,70.15);
+
+   glVertex3f(-75,-40,70.15);
+   glVertex3f(-75,-20,70.15);
+   glVertex3f(-80,-40,70.15);
+   glVertex3f(-80,-20,70.15);
+   glVertex3f(-85,-40,70.15);
+   glVertex3f(-85,-20,70.15);
+   glEnd();
+//****************************************************************************************************************************************
+//****************************************************************************************************************************************
+//JENDELA PINGGIR KIRI 1
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-100.1,40,57);
+   glVertex3f(-100.1,40,67);
+   glVertex3f(-100.1,20,67);
+   glVertex3f(-100.1,20,57);
+   glEnd();
+   glBegin(GL_LINE_LOOP);
+   glColor3f(0,0,0);
+   glVertex3f(-100.15,40,57);
+   glVertex3f(-100.15,40,67);
+   glVertex3f(-100.15,20,67);
+   glVertex3f(-100.15,20,57);
+   glEnd();
+   glBegin(GL_LINES);
+   glColor3f(0,0,0);
+   glVertex3f(-100.15,35,57);
+   glVertex3f(-100.15,35,67);
+   glVertex3f(-100.15,40,62);
+   glVertex3f(-100.15,20,62);
+   glEnd();
+//JENDELA PINGGIR KIRI 2
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-100.1,10,57);
+   glVertex3f(-100.1,10,67);
+   glVertex3f(-100.1,-10,67);
+   glVertex3f(-100.1,-10,57);
+   glEnd();
+   glBegin(GL_LINE_LOOP);
+   glColor3f(0,0,0);
+   glVertex3f(-100.15,10,57);
+   glVertex3f(-100.15,10,67);
+   glVertex3f(-100.15,-10,67);
+   glVertex3f(-100.15,-10,57);
+   glEnd();
+   glBegin(GL_LINES);
+   glColor3f(0,0,0);
+   glVertex3f(-100.15,5,57);
+   glVertex3f(-100.15,5,67);
+   glVertex3f(-100.15,10,62);
+   glVertex3f(-100.15,-10,62);
+   glEnd();
+//JENDELA PINGGIR KIRI 3
+   glBegin(GL_POLYGON);
+   glColor3f(1,1,1);
+   glVertex3f(-100.1,-40,57);
+   glVertex3f(-100.1,-40,67);
+   glVertex3f(-100.1,-20,67);
+   glVertex3f(-100.1,-20,57);
+   glEnd();
+   glBegin(GL_LINE_LOOP);
+   glColor3f(0,0,0);
+   glVertex3f(-100.15,-40,57);
+   glVertex3f(-100.15,-40,67);
+   glVertex3f(-100.15,-20,67);
+   glVertex3f(-100.15,-20,57);
+   glEnd();
+   glBegin(GL_LINES);
+   glColor3f(0,0,0);
+   glVertex3f(-100.15,-25,57);
+   glVertex3f(-100.15,-25,67);
+   glVertex3f(-100.15,-40,62);
+   glVertex3f(-100.15,-20,62);
+   glEnd();
+//****************************************************************************************************************************************
+//****************************************************************************************************************************************
+//KIRI BAWAH (MERAH)
+//KIRI
+   glBegin(GL_POLYGON);
+   glColor3f(0.97,0,0);
+   glVertex3f(-100,-50,70);
+   glVertex3f(-100,-50,50);
+   glVertex3f(-100,-80,50);
+   glVertex3f(-100,-80,70);
+   glEnd();
+//DEPAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.97,0,0);
+   glVertex3f(-60,-50,70);
+   glVertex3f(-100,-50,70);
+   glVertex3f(-100,-80,70);
+   glVertex3f(-60,-80,70);
+   glEnd();
+//KANAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.97,0,0);
+   glVertex3f(-60,-50,70);
+   glVertex3f(-60,-50,50);
+   glVertex3f(-60,-80,50);
+   glVertex3f(-60,-80,70);
+   glEnd();
+
+//****************************************************************************************************************************************
+//****************************************************************************************************************************************
+//KIRI BAWAH (MERAH) (BELAKANG)
+//KIRI
+   glBegin(GL_POLYGON);
+   glColor3f(0.97,0,0);
+   glVertex3f(-100,-50,-70);
+   glVertex3f(-100,-50,-50);
+   glVertex3f(-100,-80,-50);
+   glVertex3f(-100,-80,-70);
+   glEnd();
+//DEPAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.97,0,0);
+   glVertex3f(-60,-50,-70);
+   glVertex3f(-100,-50,-70);
+   glVertex3f(-100,-80,-70);
+   glVertex3f(-60,-80,-70);
+   glEnd();
+//KANAN
+   glBegin(GL_POLYGON);
+   glColor3f(0.97,0,0);
+   glVertex3f(-60,-50,-70);
+   glVertex3f(-60,-50,-50);
+   glVertex3f(-60,-80,-50);
+   glVertex3f(-60,-80,-70);
+   glEnd();	
 	
 	
 	
